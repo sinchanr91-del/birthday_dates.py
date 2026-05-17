a20b953989d99f052e241913bd976a4b3186e9f3
sinchanr91-del <sinchanr91@gmail.com>
2026-05-17 10:54:24 +0100

    Add files via upload

---
+++ birthday_dates.py
@@ -0,0 +1,24 @@
+Name = "Ayan"
+Birthday = "20th October 2015"
+Age = 11
+print (Name,"your age is", Age, "your birthday is", Birthday)
+
+Name = "Sampriti"
+Birthday = "20th April 2016"
+Age = 10
+print (Name,"your age is", Age, "your birthday is", Birthday)
+
+Name = "Samriddhi"
+Birthday = "18th July 2018"
+Age = 8
+print (Name,"your age is", Age, "your birthday is", Birthday)
+
+Name = "Emon"
+Birthday = "2nd January 2015"
+Age = 11
+print (Name,"your age is", Age, "your birthday is", Birthday)
+
+Name = "Sanjith"
+Birthday = "7th July 2015"
+Age = 10
+print (Name,"your age is", Age, "your birthday is", Birthday)
